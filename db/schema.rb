aae99e1c8180f2eb2e37da4f0a44ee3c59c25265
Bohdan Vakh <Bohdanvah@gmail.com>
2023-11-07 21:32:47 +0200

    feat/updated gemfile

--- db/schema.rb
+++ db/schema.rb
@@ -57,12 +57,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_13_142810) do
     t.string "phone_number"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "category_id"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer "category_id"
     t.index ["category_id"], name: "index_doctors_on_category_id"
     t.index ["email"], name: "index_doctors_on_email", unique: true
     t.index ["reset_password_token"], name: "index_doctors_on_reset_password_token", unique: true
