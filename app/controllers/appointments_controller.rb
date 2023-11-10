1725e70cb90a82f2e0a136113196f4d6ce6ea168
Bohdan Vakh <Bohdanvah@gmail.com>
2023-11-10 21:01:32 +0200

    feat/add add recommendation feature for doctor

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -19,9 +19,25 @@ class AppointmentsController < ApplicationController
     @appointment = Appointment.find(params[:id])
   end
 
+  def edit
+    @appointment = Appointment.find(params[:id])
+  end
+
+  def update
+    @appointment = Appointment.find(params[:id])
+
+    if current_doctor.id == @appointment.doctor_id
+      if @appointment.update(appointment_params)
+        redirect_to doctors_index_path(current_doctor.id), alert: "Appointment Updated"
+      else
+        redirect_to doctors_index_path(current_doctor.id), alert: "Appointment wasn't Updated"
+      end
+    end
+  end
+
   private
 
   def appointment_params
-    params.require(:appointment).permit(:doctor_id, :patient_id, :active_status, :date)
+    params.require(:appointment).permit(:doctor_id, :patient_id, :active_status, :date, :recommendation)
   end
 end
